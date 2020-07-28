e60a9ded21d48c1865be00145ff59ca4f0cc410c
kimikage <kimikage.ceo@gmail.com>
2020-07-28 14:07:52 +0900

    Add docstring and doctests
    This also applies some trivial modifications of source code, which do not change the behavior.

--- docs/make.jl
+++ docs/make.jl
@@ -5,8 +5,12 @@ makedocs(
     sitename = "Graphics",
     format = Documenter.HTML(prettyurls = get(ENV, "CI", nothing) == "true"),
     modules = [Graphics],
-    pages = ["index.md", "reference.md"]
+    pages = ["index.md", "reference.md"],
+    clean = false,
+    checkdocs = :exports
 )
+
+DocMeta.setdocmeta!(Graphics, :DocTestSetup, :(using Graphics); recursive=true)
 
 deploydocs(
     repo = "github.com/JuliaGraphics/Graphics.jl.git"
